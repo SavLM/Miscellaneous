07cb3ca4a5808ff52d13f14ce7bb70aa0d16b27b
SavLM <50849597+SavLM@users.noreply.github.com>
2020-02-09 19:13:04 -0500

    added cloudwatch timing randomization

--- goatsms/goatsms.py
+++ goatsms/goatsms.py
@@ -1,10 +1,22 @@
 from twilio.rest import Client
 import os
+from random import randint
+import boto3
+
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
 account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
 auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
+
+def change_time():
+    value = randint(12,17)
+    schedule = 'cron(0 ' + value + ' * * ? *)'
+    sched = boto3.client('events')
+    sched.put_rule(
+        Name='goattime',
+        ScheduleExpression=schedule,
+    )
 
 def lambda_handler(event, context):
     ret = ''
@@ -18,4 +30,5 @@ def lambda_handler(event, context):
                 to=number
             )
         ret += message.sid + ' '
+    change_time()
     return ret
